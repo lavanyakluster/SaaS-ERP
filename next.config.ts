8b3ee3443b06b6d619bbc0a3b5a4f009aab5d8d7
lavanya <lavanya@klusterco.com>
2026-02-08 18:57:52 +0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
 
   webpack: (config: Configuration, options) => {
     const { dev, isServer } = options;
